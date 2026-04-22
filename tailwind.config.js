3c725419cc39275125e571c22da855b9c05cf83f
Kalai Arasan <145125390+Kalaiarasan-Dev@users.noreply.github.com>
2026-04-22 16:57:11 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'spin': 'spin 1s linear infinite',
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
